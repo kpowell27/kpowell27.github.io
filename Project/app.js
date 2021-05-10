ee38a1d98371451db46891c7d0426c8a68348181
Kayla Powell <kaylapowell@Kaylas-MacBook-Pro.local>
2021-05-10 11:50:42 -0500

    shuffle funtion

--- Project/app.js
+++ Project/app.js
@@ -13,4 +13,14 @@ for (let i = 0; i < suits.length; i++) {
     }
 }
 // console.log(deckOfCards)
+// Shuffle card funtion 
+// used (https://medium.com/swlh/the-javascript-shuffle-62660df19a5d, for the shuffle funtion)
+for (let i = deckOfCards.length - 1; i > 0; i--) {
+  let x = Math.floor(Math.random() * i);
+  let temp = deckOfCards[i];
+  deckOfCards[i] = deckOfCards[x];
+  deckOfCards[x] = (temp);
+// }
+// console.log(deckOfCards)
 
+console.log(deckOfCards.pop());
