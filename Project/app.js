ad087a530712523ff417e2ba69ad46b3b569af6f
Kayla Powell <kaylapowell@Kaylas-MBP.cable.rcn.com>
2021-05-12 17:07:34 -0500

    blackjack game class and player

--- Project/app.js
+++ Project/app.js
@@ -7,6 +7,14 @@
 // Game has deck, player, dealer
 // deck has card 
 //dealer is player 
+// player gets two cards
+// dealer gets two cards 
+// player has the option to take another card or stay without reaching over 21 
+// if player card is over 21 - player loses
+// if players card is under 21 they have the option to stay or hit 
+// dealer shows second card that is hidden 
+// dealer must keep pulling a card until reaches 17
+// dealer loses if card excessed over 21 
 
 
 class Card {
@@ -15,8 +23,7 @@ class Card {
       this.suit = suit.toLowerCase();
   }
 
-
-  // all card's secondary value is equal to their primary value, except the `ace` card
+ // all card's secondary value is equal to their primary value, except the `ace` card
   getCardValue() {
       if(this.rank=="king") {
           return 10;
@@ -26,7 +33,10 @@ class Card {
           return 10;
       } else if(this.rank == "ace") {
           return 11;
+      }else{
+         return this.rank;
       }
+
   }
 }
 
@@ -79,10 +89,132 @@ class BlackJackPlayer {
       console.log(this.getHandTotal());
   }
 
-  // add card to hand (presumably from deck or dealer)
+  // add card to hand from deck or dealer)
   hit(incomingCard) {
       this.cards.push(incomingCard);
   }
 
+  getHandTotal() {
+    let total = 0;
+    this.cards.forEach(currentCard=> {
+        let currentCardValue = currentCard.getCardValue();
+        total += currentCardValue
+    })
+    return total;
+  }
+
 
 }
+
+class BlackJackGame {
+  constructor() {
+    
+      this.player = new BlackJackPlayer('Player 1');
+      this.dealer = new BlackJackPlayer('Kayla');
+  }
+
+  play() {
+      const deck = new Deck();
+      deck.shuffle();
+      let dealerCard1 = deck.removeAndFetchTopMostCard();
+      let dealerCard2 = deck.removeAndFetchTopMostCard();
+      this.dealer.hit(dealerCard1);
+      this.dealer.hit(dealerCard2);
+
+      let playerCard1 = deck.removeAndFetchTopMostCard();
+      let playerCard2 = deck.removeAndFetchTopMostCard();
+      this.player.hit(playerCard1);
+      this.player.hit(playerCard2);
+
+      console.log('Dealers hand' );
+      this.dealer.viewHand();
+      
+      console.log('Players hand' );
+      this.player.viewHand();
+
+      let dealerHandTotal = this.dealer.getHandTotal();
+      let playerHandTotal = this.player.getHandTotal();
+      let isPush = dealerHandTotal == playerHandTotal;
+
+      if(isPush){
+        console.log('push')
+      }
+    
+      else if(dealerHandTotal > 21) {
+        console.log("dealer bust");
+      }
+
+      else if(playerHandTotal > 21) {
+        console.log("Player bust");
+      }
+
+      else if(dealerHandTotal == 21) {
+        console.log("dealer wins");
+      } 
+
+      else if(playerHandTotal == 21) {
+          console.log("player wins")
+      }
+      
+
+
+    // Players decision 
+      while (this.player.getHandTotal() <21){
+        let hitOrStand = prompt('Would you like to hit or stand');
+        if (hitOrStand == 'hit') {
+          let topCard = deck.removeAndFetchTopMostCard();
+          this.player.hit(topCard);
+        } 
+        else {
+          break;
+        }
+      }
+
+
+      //Dealers decision
+      while (this.dealer.getHandTotal() <17){
+        let topCard = deck.removeAndFetchTopMostCard();
+          this.player.hit(topCard);
+      }
+      console.log("After decision");
+      this.player.viewHand();
+      this.dealer.viewHand();
+      
+
+      dealerHandTotal = this.dealer.getHandTotal();
+      playerHandTotal = this.player.getHandTotal();
+      isPush = dealerHandTotal == playerHandTotal;
+      
+      if(isPush){
+        console.log('push');
+      }
+    
+      else if(dealerHandTotal > 21) {
+        console.log("dealer bust");
+      }
+
+      else if(playerHandTotal > 21) {
+        console.log("Player bust");
+      }
+
+      else if(dealerHandTotal == 21) {
+        console.log("dealer wins");
+      } 
+
+      else if(playerHandTotal == 21) {
+          console.log("player wins")
+      }
+      else{
+        console.log("ERROR refresh page and start again")
+      }
+
+
+  }
+}
+  // given
+  let blackJackGame = new BlackJackGame();
+  
+  // when    
+  blackJackGame.play();
+  
+  
