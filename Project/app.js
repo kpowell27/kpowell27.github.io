92c4f85be2c240aece3a3efad750ad8d077f5e93
Kayla Powell <kaylapowell@Kaylas-MBP.cable.rcn.com>
2021-05-12 12:53:58 -0500

    update

--- Project/app.js
+++ Project/app.js
@@ -5,17 +5,15 @@
 //creater a player 
 
 
-
 const suits=["Hearts", "Spades", "Diamonds", "Clubs"]
 const values = ['Ace', 'King', 'Queen', 'Jack',
   '10', '9', '8', '7', '6',
   '5', '4', '3', '2', '1'
 ];
-//Deck of cards funtion 
-// const deck = new deck(suits, value);
+
 let deckOfCards =[];
-let deck = newDeck();
-deck.createDeck(suits, values)
+// deckOfCards.createnewDeckOfCards(suits, values);
+//Deck of cards funtion 
 
 for (let i = 0; i < 3; i++) {
     for (let x = 0; x < values.length; x++) {
@@ -24,38 +22,18 @@ for (let i = 0; i < 3; i++) {
     }
 }
 // console.log(deckOfCards)
+
 // Shuffle card funtion 
 // used (https://medium.com/swlh/the-javascript-shuffle-62660df19a5d, for the shuffle funtion)
-for (let i = deckOfCards.length - 1; i > 4; i--) {
+
+for (let i = deckOfCards.length - 1; i > 1; i--) {
   let x = Math.floor(Math.random() * i);
   let temp = deckOfCards[i];
   deckOfCards[i]  = deckOfCards[x];
   deckOfCards[x] = (temp);
-  
-  deal()  
-    let hand  = [];
-    while (hand.length < 2){
-      hand.push(this.deck.pop());
-    }
-   return hand;
-  
 }
+  
 console.log(deckOfCards)
+ 
+// deck 
 
-class Deck {
-  constructor()
-  this.deck = [];
-}
-
-createDeck(suits, vaules) {
-  for(let suit of suits) {
-    for (let value of values)
-    this.deck.push(new Card(suits, value))
-  }
-}
-return this.deck;
-
-
-
-
-console.log(deck.deal)
