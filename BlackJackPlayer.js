fb2662b5459826eeaf0153f768c1b3a390913bff
Kayla Powell <kaylapowell@Kaylas-MBP.cable.rcn.com>
2021-05-14 06:39:43 -0500

    update

---
+++ BlackJackPlayer.js
@@ -0,0 +1,36 @@
+class BlackJackPlayer {
+    // a blackjack player should receive a name when created
+    // a black jack player's hand is empty until receiving cards from a dealer
+    constructor(name) {
+        this.name = name;
+        this.cards = [];
+    }
+
+    // prints the cards of the current BlackJackPlayer
+    viewHand() {
+        console.log("Printing the hand of player [" + this.name + "]");
+        this.cards.forEach(card => {
+            console.log(card.toString());
+        });
+    }
+
+    // add card to hand (presumably from deck or dealer)
+    hit(deck) {
+        let incomingCard = deck.removeAndFetchTopMostCard();
+        this.addCard(incomingCard);
+    }
+    
+    addCard(cardToAddToHand) {
+        this.cards.push(cardToAddToHand);
+    }
+
+    getHandTotal() {
+        let total = 0;
+        this.cards.forEach(currentCard=> {
+            let currentCardValue = currentCard.getValue();
+            total += currentCardValue
+        })
+        return total;
+    }
+ 
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-my site can be found here https://kpowell27.github.io 
+# Simple Arcade Simulation
+* **Objective** - To create a product which
+* **Purpose** - To gain familiarity with
+* **Description** - Blah
+
+
+## Instructions
+* Upon cloning this project execute `node app.mjs` from the root directory of the project to view the console-based output
